1aa91c7e451f03a627cf81546742d9952e82b324
Brian Henry <brianhenrytech@gmail.com>
2025-03-06 14:43:51 -0600

    Fixed `Postlist` component time display

--- src/components/posts/PostList.js
+++ src/components/posts/PostList.js
@@ -1,62 +1,67 @@
-import { useEffect, useState } from "react"
-import { getAllPosts, getAllPostTags } from "../../managers/PostManager"
-import { getAllCategories } from "../../managers/CategoryManager"
-import { getAllTags } from "../../managers/TagManager"
-
+import { useEffect, useState } from "react";
+import { getAllPosts, getAllPostTags } from "../../managers/PostManager";
+import { getAllCategories } from "../../managers/CategoryManager";
+import { getAllTags } from "../../managers/TagManager";
+import { HumanDate } from "../utils/HumanDate";
 
 export const PostList = () => {
-    const [posts, setPosts] = useState([])
-    const [categories, setCategories] = useState([])
-    const [tags, setTags] = useState([])
-    const [postTags, setPostTags] = useState([])
-    const [testing, setTesting] = useState([])
+  const [posts, setPosts] = useState([]);
+  const [categories, setCategories] = useState([]);
+  const [tags, setTags] = useState([]);
+  const [postTags, setPostTags] = useState([]);
+  const [testing, setTesting] = useState([]);
 
-    useEffect(() => {
-        getAllPosts().then(postsArray => {
-            setPosts(postsArray)
-        })
-        getAllCategories().then(categoriesArray => {
-            setCategories(categoriesArray)
-        })
-        getAllTags().then(tagsArray => {
-            setTags(tagsArray)
-        })
-        getAllPostTags().then(postTagsArray => {
-            setPostTags(postTagsArray)
-        })
-    }, [])
+  useEffect(() => {
+    getAllPosts().then((postsArray) => {
+      setPosts(postsArray);
+    });
+    getAllCategories().then((categoriesArray) => {
+      setCategories(categoriesArray);
+    });
+    getAllTags().then((tagsArray) => {
+      setTags(tagsArray);
+    });
+    getAllPostTags().then((postTagsArray) => {
+      setPostTags(postTagsArray);
+    });
+  }, []);
 
+  return (
+    <div>
+      {posts.map((post) => {
+        let postCategory = categories.find(
+          (category) => category.id === post.category_id,
+        );
 
-    return (
-        <div>
-            {posts.map((post) => {
+        let postTagArr = postTags.filter(
+          (postTag) => postTag.post_id === post.id,
+        );
 
-                let postCategory = categories.find(category => category.id === post.category_id)
+        let relatedTags = postTagArr.flatMap((postTag) =>
+          tags.filter((tag) => tag.id === postTag.tag_id),
+        );
+        console.log(relatedTags.map((tag) => tag.label));
 
-                let postTagArr = postTags.filter(postTag => postTag.post_id === post.id)
-                
-                let relatedTags = postTagArr.flatMap(postTag =>
-                    tags.filter(tag => tag.id === postTag.tag_id)
-                );
-                console.log(relatedTags.map(tag => tag.label))
-
-                return (
-                    <div key={post.id}>
-                        <div>
-                            <div>Title</div>
-                            <div>{post.title}</div>
-                            <div>Author</div>
-                            <div>Date</div>
-                            <div>{post.publication_date}</div>
-                            <div>Category</div>
-                            <div>{postCategory ? postCategory.label : ""}</div>
-                            <div>Tags</div>
-                            <div>{ relatedTags ? relatedTags.map(tag => tag.label).join(", ") : ""}</div>
-                        </div>
-                    </div>
-                );
-            })}
-        </div>
-    )
-}
-
+        return (
+          <div key={post.id}>
+            <div>
+              <div>Title</div>
+              <div>{post.title}</div>
+              <div>Author</div>
+              <div>Date</div>
+              <div>{HumanDate(post.publication_date.split("T")[0])}</div>
+              <div>Category</div>
+              <div>{postCategory ? postCategory.label : ""}</div>
+              <div>Tags</div>
+              <div>
+                {relatedTags
+                  ? relatedTags.map((tag) => tag.label).join(", ")
+                  : ""}
+              </div>
+            </div>
+          </div>
+        );
+      })}
+    </div>
+  );
+};
