742e6654f775d32cbb778267f29009842caa97ad
Ethan Brown <eb934053@gmail.com>
2025-03-10 16:22:18 -0500

    Added DELETE functionality in My Posts component

--- src/components/posts/PostList.jsx
+++ src/components/posts/PostList.jsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from "react";
 import { getAllPosts, getAllPostTags } from "../../managers/PostManager";
 import { getAllCategories } from "../../managers/CategoryManager";
 import { getAllTags } from "../../managers/TagManager";
-import { getPostsByUserId } from "../../managers/PostServices";
+import { deletePost, getPostsByUserId } from "../../managers/PostServices";
 
 import { HumanDate } from "../utils/HumanDate";
 import "../../Rare.css";
@@ -13,7 +13,7 @@ export const PostList = ({ token }) => {
   const [postTags, setPostTags] = useState([]);
   const [filterCategory, setFilteredCategory] = useState(posts);
 
-  useEffect(() => {
+  const getAndSetPosts = () => {
     getAllPosts().then((postsArray) => {
       setPosts(postsArray);
     });
@@ -29,14 +29,20 @@ export const PostList = ({ token }) => {
     if (token) {
       getPostsByUserId(token).then((postArray) => setPosts(postArray));
     }
-  }, [token]);
+  }
+
+  useEffect(() => {
+    getAndSetPosts()
+  }, [token])
 
   useEffect(() => {
     setFilteredCategory(posts);
   }, [posts]);
 
-  const handleDeletePost = () => {
-    console.log("Post Deleted!");
+  const handleDeletePost = (event) => {
+    event.preventDefault()
+    console.log(event.target.id)
+    deletePost(parseInt(event.target.id)).then(getAndSetPosts)
   };
 
   const handleEditPost = () => {
@@ -110,18 +116,18 @@ export const PostList = ({ token }) => {
                       ? relatedTags.map((tag) => tag.label).join(", ")
                       : ""}
                   </div>
-                  <div className="is-pulled-right">
+                  <div className="is-pulled-right fa-solid fa-edit">
                     <button
                       className="button is-primary"
+                      id={post.id}
                       onClick={handleEditPost}
                     >
-                      <i className="fa-solid fa-edit"></i>
                     </button>
                     <button
-                      className="button is-danger"
+                      className="button is-danger fa-solid fa-trash-can"
+                      id={post.id}
                       onClick={handleDeletePost}
                     >
-                      <i className="fa-solid fa-trash-can"></i>
                     </button>
                   </div>
                 </footer>
