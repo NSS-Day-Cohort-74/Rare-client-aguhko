177b976f21b7486e88ddccd83949da015fcd3458
Henry Parrish <henrybparrish@gmail.com>
2025-03-13 14:50:50 -0600

    fix: merge with development

--- src/managers/CommentManager.js
+++ src/managers/CommentManager.js
@@ -1,3 +1,9 @@
 export const getAllComments = () => {
 	return fetch("http://localhost:8088/comments").then((res) => res.json());
 };
+export const deleteComment = (commentId) => {
+    console.log(commentId)
+    return fetch(`http://localhost:8088/comments/${commentId}`, {
+        method: "DELETE"
+    })
+}
